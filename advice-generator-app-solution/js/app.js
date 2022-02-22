924bb66beded3749ac74889a460b9fbfbcb29145
Uzair Ahmed <uzairahmed74372@gmail.com>
2022-02-23 00:55:41 +0500

    fixed fetching problem

--- advice-generator-app-solution/js/app.js
+++ advice-generator-app-solution/js/app.js
@@ -2,21 +2,16 @@ const changeAdvice = document.getElementById("changeAdvice");
 const heading = document.querySelector("h1");
 const advice = document.querySelector("q");
 
-console.log(heading.innerHTML);
-
 const API = "https://api.adviceslip.com/advice";
 
-changeAdvice.addEventListener("click", () => {
-    changeAdvice.style.animationName =  "rotate";
-    changeAdvice.style.animationDuration = '.4s';
-    changeAdvice.style.animationIterationCount = "infinite";
-  fetch(API)
-    .then((res) => res.json())
-    .then((data) => {
-      heading.innerText = `ADVICE #${data.slip.id}`;
-      advice.innerText = data.slip.advice;
-      changeAdvice.style.animationName =  "";
-      changeAdvice.style.animationDuration = '';
-      changeAdvice.style.animationIterationCount = "";
-    });
+changeAdvice.addEventListener("click", async () => {
+  const random = Math.floor(Math.random() * 224) + 1;
+  changeAdvice.classList.add("rotate-animation");
+  setTimeout(() => {
+    changeAdvice.classList.remove("rotate-animation");
+  }, 1000);
+  const res = await fetch(`${API}/${random}`)
+  const data = await res.json()
+  heading.innerText = `ADVICE #${data.slip.id}`;
+  advice.innerText = data.slip.advice;
 });
